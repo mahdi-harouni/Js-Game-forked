1f4dc2f587d4b95f3015bd7b8d94db50cf01e96c
Hamed Moodi <hamedmoodi2011@gmail.com>
2023-05-31 17:01:24 +0330

    update

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ $(document).ready(function(){
         }
     }
 
+    
+
     function send_message( text ){
         $.post('https://notificator.ir/api/v1/send', {
             to      : 'B1zLOsSjM7EV9YlQ5X8UHkBFsUZTsVdXtcnd9bMc',
